73f5cda5b7fbd3c5908b5ff385003d83e232b5a4
ferhat <ferhat.mehdioui@monboreal.ca>
2026-03-08 06:21:59 -0400

    implementation des methodes de employe

--- atl4.py
+++ atl4.py
@@ -5,3 +5,30 @@ class Employee:
         self.prenom=prenom
         self.etat = False
         self.voiture = None
+    def afficher_Info(self):
+        print(f" l'employer : {self.nom} {self.prenom} a un permis avec ce numero : {self.numero_Permis}")
+        if self.etat == True:
+            print(f"il a pris la voiture matricule avec : {self.voiture.matricule}")
+
+        else:
+            print("il n'a pas pris une voiture !")
+
+    def affecter_Voiture(self,voiture):
+        if self.etat == True:
+            print("il a deja  pris une voiture !")
+        else :
+            self.voiture = voiture
+            self.etat = True
+            print("voiture affectee a votre employe !")
+            voiture.chauffeur = self
+    def retirer_voiture(self,voiture):
+        if self.etat == True:
+            self.etat = False
+            print ("voiture retiree a votre employe !")
+        else :
+            print("il a pas pris cette voiture pour la retirer!")
+
+
+
+
+
