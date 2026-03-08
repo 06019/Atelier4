85d212f18c230858bc4487768796d5ee5ad972b8
ferhat <ferhat.mehdioui@monboreal.ca>
2026-03-08 01:53:38 -0500

    creation de class Employe

---
+++ atl4.py
@@ -0,0 +1,7 @@
+class Employee:
+    def __init__(self, numPerm, nom,prenom,):
+        self.numero_Permis = numPerm
+        self.nom=nom
+        self.prenom=prenom
+        self.etat = False
+        self.voiture = None
